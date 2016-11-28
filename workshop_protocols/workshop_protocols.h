d547ebacc89042dd7995c0192df4ea78291da41e
Jordi Aguila <jaguila@mobbio.com>
2016-11-28 13:24:29 +0100

    Initial commit

---
+++ workshop_protocols/workshop_protocols.h
@@ -0,0 +1,19 @@
+//
+//  workshop_protocols.h
+//  workshop_protocols
+//
+//  Created by Jordi Aguila on 28/11/16.
+//  Copyright © 2016 Napptilus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for workshop_protocols.
+FOUNDATION_EXPORT double workshop_protocolsVersionNumber;
+
+//! Project version string for workshop_protocols.
+FOUNDATION_EXPORT const unsigned char workshop_protocolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <workshop_protocols/PublicHeader.h>
+
+
